3fa44add14ffd14243513e4c106c11d367951d85
Issei.M <issei.m7@gmail.com>
2013-11-24 00:54:54 +0900

    supported opcache and more configurations

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,8 @@
 
 default['php']['packages'] = ['php', 'php-mbstring', 'php-intl', 'php-pecl-zendopcache']
 
+default['php']['config']['display_errors'] = 'Off'
+
 default['php']['config']['date']['timezone'] = 'Asia/Tokyo'
 
 default['php']['config']['default_charset'] = 'UTF-8'
@@ -18,4 +20,12 @@ default['php']['config']['realpath_cache_ttl'] = '7200'
 default['php']['config']['mbstring'] = {
   :language => 'Japanese',
   :internal_encoding => 'UTF-8'
+}
+
+default['php']['config']['opcache'] = {
+  :memory_consumption => '128',
+  :interned_strings_buffer => '8',
+  :max_accelerated_files => '4000',
+  :revalidate_freq => '2',
+  :fast_shutdown => 'On'
 }
